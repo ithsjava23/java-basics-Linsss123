b1b52ba04c29193b43d2ca8371f61b111a218524
smull <smulle.junior@gmail.com>
2023-09-15 13:35:55 +0200

    Add code for reading input

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,9 +1,16 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
-        String menu = """
+        Scanner scanner = new Scanner(System.in);
+
+        String choice;
+
+        do {
+            String menu = """
                 Elpriser
                 ========
                 1. Inmatning
@@ -12,6 +19,12 @@ public class App {
                 4. Bästa Laddningstid (4h)
                 e. Avsluta
                 """;
-        System.out.print(menu);
+
+            System.out.print(menu);
+            choice = scanner.nextLine();
+            //Add code for choices 1 to 4
+
+        }while(!choice.equalsIgnoreCase("e"));
+
     }
 }
